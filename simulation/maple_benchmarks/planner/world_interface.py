22fbee92df190ef6e46b2c9bfb75dbd6a73321da
FaseehCS <faseeh.ahmad@cs.lth.se>
2024-10-03 11:00:11 +0200

    New: Helper functions for planning interface
    - set_object_pos --> sets the position of an object
    - object_at_pos --> checks if object is at a position

--- simulation/maple_benchmarks/planner/world_interface.py
+++ simulation/maple_benchmarks/planner/world_interface.py
@@ -30,7 +30,9 @@
 # CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
 # THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+import numpy as np
 
+IN_POS_DIST = 0.03  # Distance to consider in position for reach targets
 
 class WorldInterface:
     """
@@ -54,7 +56,7 @@ class WorldInterface:
         self.graspable_objects = graspable_objects
         self.door_open = False
         self.scenario = scenario
-
+        # Modify to save the failed actions, etc. Uses pytree information(indirectly)
     def get_feedback(self):
         # pylint: disable=no-self-use
         """ Dummy to fit template """
@@ -83,6 +85,34 @@ class WorldInterface:
             return True
         return False
 
+    def set_object_pos(self, target_object, pos):
+        """ Sets the position of an object"""
+        self.object_positions[target_object] = pos
+
+    def object_at_pos(self, target_object, pos, threshold=0.0):
+        """ Is target object currently at the target position"""
+        if threshold == 0.0:
+            threshold = IN_POS_DIST
+        object_position = self.object_positions[target_object]
+        if object_position == target_object+'_INITIAL':  # default position is string
+            return False
+        
+        # Convert object_position and pos to tuples of floats
+        object_position = eval(self.object_positions[target_object]) if isinstance(self.object_positions[target_object], str) else self.object_positions[target_object]
+        if pos == '':
+            print ("parameters[2] is empty. returning False")
+            return False
+        pos = eval(pos) if isinstance(pos, str) else pos
+        # Convert to numpy arrays for subtraction and distance calculation
+        object_position_array = np.array(object_position, dtype=float)
+        pos_array = np.array(pos, dtype=float)
+        
+        # Perform the subtraction and check if the distance is within the threshold
+        if np.linalg.norm(object_position_array - pos_array) < threshold:
+            # print("Hole is blocked by", target_object, np.linalg.norm(object_position_array - pos_array), threshold)
+            return True
+        return False
+    
     def is_door_open(self):
         """ True if door is open """
         return self.door_open
